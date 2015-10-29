c3d709f81d09eb49a479c210ae5105e2bcd05d4c
Mike Rosack <mrosack@gmail.com>
2015-10-28 21:41:04 -0500

    return 400 response for an invalid address

---
+++ CivicHackApiLocator.WebApi/Models/GenericErrorResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CivicHackApiLocator.WebApi.Models
+{
+    /// <summary>
+    /// A generic format for returning errors
+    /// </summary>
+    public class GenericErrorResponse
+    {
+        /// <summary>
+        /// Gets or sets the error message
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
